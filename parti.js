3946138f18eac521c175f4313b0ac76d0b5e1449
potterheadquiditch <74307161+potterheadquiditch@users.noreply.github.com>
2021-01-12 16:49:10 +0530

    Update parti.js

--- parti.js
+++ parti.js
@@ -2,7 +2,7 @@ class parti{
     constructor(x,y,radius){
          var options={
             
-          restitution:0.1    
+          restitution:0.6    
            } 
            this.body=Bodies.circle(x,y,radius,options);
             this.x=x; 
